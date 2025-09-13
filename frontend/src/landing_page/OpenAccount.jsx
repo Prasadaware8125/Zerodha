cc3ad69197e57f751efbc4982af5ffc77551384a
Prasadaware8125 <prasadaware6613@gmail.com>
2025-09-13 12:57:28 +0530

    Completed Home/Landing page

---
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function OpenAccount() {
+  return (
+    <div className="container p-5 mb-5">
+        <div className="row text-center">
+          <h1 className='mt-2'>Open a Zerodha account</h1>
+          <p>Modern platform and apps, &#8377;0 Investments, and flat &#8377;20 intraday and F&O trades.</p>
+          <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"25%", margin:"0 auto"}}>Sign up now</button>
+        </div>
+      </div>
+  )
+}
+
+export default OpenAccount
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import HomePage from './landing_page/Home/HomePage'
+import Navbar from './landing_page/Navbar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <App/>
+      <HomePage/>
     </>
   </StrictMode>,
 )
