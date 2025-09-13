0de166d6a4e4f4b15c829bd053355e7c30b380e0
Prasadaware8125 <prasadaware6613@gmail.com>
2025-09-13 13:49:00 +0530

    Completed Routing

---
+++ frontend/src/landing_page/About/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="container">
+        <div className="row p-5">
+            <h1 className='text-center fs-2'>
+                We pioneered the discount broking model in India. <br />Now, we are breaking ground with our technology.
+            </h1>
+        </div>
+        <div className="row">
+            <div className="col"></div>
+            <div className="col"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ frontend/src/landing_page/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div className="container text-center p-5">
+        <div className="row">
+            <h1>404 Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
